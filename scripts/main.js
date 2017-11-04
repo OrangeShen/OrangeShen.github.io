766bcfc597a846983e1ec4447438fc68f4b2f069
OrangeShen <897139816@qq.com>
2017-11-04 22:53:52 +0800

    small changes
    1. change background image for login page;
    2. enhance sign up function

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,11 @@ var UP = {};
 var div = document.getElementById("div");
 var username = document.getElementById("username");
 var passwd = document.getElementById("passwd");
-var warn = document.getElementById("warning")
+var warn = document.getElementById("warning");
 var signin = document.getElementById("button").firstElementChild;
 var signup = document.getElementById("button").lastElementChild;
 
-warn.innerText = "if you haven't sign up,enter your username and password and click sign up. if you already signed up, use your username and password to sign up.";
+warn.innerText = "if you haven't sign up, enter your username and password and click sign up. if you already signed up, use your username and password to sign in.";
 
 signin.onclick = function() {
     var uv = username.value;
@@ -26,12 +26,20 @@ signin.onclick = function() {
 }
 
 signup.onclick = function() {
-    if (!UP.hasOwnProperty(uv)) {
-        var uv = username.value;
-        var pv = passwd.value;
-        UP[uv] = pv;
-        warn.innerText = "sign up successfully!";
+    var uv = username.value;
+    var pv = passwd.value;
+    if (uv) {
+        if (!UP.hasOwnProperty(uv)) {
+            if (pv) {
+                UP[uv] = pv;
+                warn.innerText = "sign up successfully!";
+            }else {
+                warn.innerText = "password can not be empty!";
+            }
+        }else {
+            warn.innerText = "you have already signed up";
+        }
     }else {
-        warn.innerText = "you have already signed up";
+        warn.innerText = "username can not be empty!";
     }
 }
